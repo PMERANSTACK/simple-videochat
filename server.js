2a20fc1859ebba72669068d66c01b088c0c51fb9
Mahendranath Reddy Bandi <fullstack.devonar@gmail.com>
2021-06-05 00:07:53 +0530

    Initial Chat-ui created

---
+++ server.js
@@ -0,0 +1,39 @@
+const  express = require('express');
+const app = express();
+const { v4 : uuidv4 } = require('uuid');
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use('/peerjs', peerServer);
+
+app.get('/' , async(req, res) => {
+    await res.redirect(`/${uuidv4()}`);
+ // await  res.render("room")
+});
+
+app.get('/:room', async (req, res)=> {
+  console.log(req.params.room);
+  await res.render('room', {roomId: req.params.room});
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit(
+      'user-connected', userId
+    );
+    socket.on('message', message => {
+      io.to(roomId).emit('createMessage', message)
+    })
+  })
+})
+
+server.listen(process.env.PORT || 3000);
